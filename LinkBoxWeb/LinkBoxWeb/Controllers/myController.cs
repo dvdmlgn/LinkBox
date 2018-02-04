792dad484a8356ea9b2d163ec3fd4e4f5b501484
david mulligan <dvdmlgn@outlook.com>
2018-02-04 00:30:57 +0000

    * myController.cs: card border not working
    * new.cs:
    * LinkBoxWeb.csproj:
    * Index.cshtml:
    * style.css:
    * _Layout.cshtml:

---
+++ LinkBoxWeb/LinkBoxWeb/Controllers/myController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LinkBoxWeb.Controllers
+{
+    public class myController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            List<my> mys = new List<my>();
+            mys.Add(new my()
+            {
+                Subject = "Maths",
+                Topic = "Algebra",
+                Chapter = "Quadratic Equations",
+
+                Description = "mathssss",
+
+                Content = "https://startbootstrap.com/template-categories/landing-pages/"
+            });
+
+            mys.Add(new my()
+            {
+                Subject = "Maths",
+                Topic = "Algebra",
+                Chapter = "Quadratic Equations",
+
+                Description = "mathssss",
+
+                Content = "https://startbootstrap.com/template-categories/landing-pages/"
+            });
+
+            mys.Add(new my()
+            {
+                Subject = "Maths",
+                Topic = "Algebra",
+                Chapter = "Quadratic Equations",
+
+                Description = "mathssss",
+
+                Content = "https://startbootstrap.com/template-categories/landing-pages/"
+            });
+
+
+            return View(mys);
+        }
+    }
+}
